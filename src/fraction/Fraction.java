d296f61cf0e4c80b8d3b0da3cf4ae91d2f062dcf
David Černý <Cerny1-D-3fb4@SKYNET.TUL.CZ>
2026-02-13 10:11:07 +0100

    init

---
+++ src/fraction/Fraction.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fraction;
+
+/**
+ *
+ * @author Cerny1-D-3fb4
+ */
+public class Fraction {
+    
+}
